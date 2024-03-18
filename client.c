4a7ee399991ef3ab42065cfacef515a8b3bd6aa4
Quang Vinh Dang <dqvinh101@gmail.com>
2024-03-18 10:58:25 -0700

    Update client code

--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 #include <sys/socket.h>
@@ -19,45 +20,9 @@ typedef struct MessageChunk {
   char buffer[BUFFER_SIZE];
 } MessageChunk;
 
-void *listen_input(void *arg) {
-
-  int sockfd = *(int *)(arg);
-  pb_ostream_t output = pb_ostream_from_socket(sockfd);
-
-  while (1) {
-
-    MessageChunk chunk;
-    memset(chunk.buffer, 0, BUFFER_SIZE);
-
-    int byte_read = read(STDIN_FILENO, chunk.buffer, BUFFER_SIZE - 1);
-
-    ChatMessage chat = {};
-    strcpy(chat.chat, chunk.buffer);
-
-    if (!pb_encode_delimited(&output, ChatMessage_fields, &chat)) {
-      fprintf(stderr, "Encoding failed: %s\n", PB_GET_ERROR(&output));
-    }
-  }
-
-  return NULL;
-}
-
-void *write_output(void *arg) {
-
-  int sockfd = *(int *)(arg);
-  char buffer[BUFFER_SIZE];
-
-  while (1) {
-    bzero(buffer, BUFFER_SIZE);
-    int byte = read(sockfd, buffer, BUFFER_SIZE - 1);
-    if (byte < 0) {
-      break;
-    }
-    printf("%s", buffer);
-  }
-
-  return NULL;
-}
+void evaluate_cmd(const char *arg);
+void *listen_input(void *arg);
+void *write_output(void *arg);
 
 int main() {
 
@@ -88,4 +53,80 @@ int main() {
   close(sockfd);
 
   return 0;
+}
+
+void *listen_input(void *arg) {
+
+  int sockfd = *(int *)(arg);
+  pb_ostream_t output = pb_ostream_from_socket(sockfd);
+
+  while (1) {
+
+    MessageChunk chunk;
+    memset(chunk.buffer, 0, BUFFER_SIZE);
+
+    int byte_read = read(STDIN_FILENO, chunk.buffer, BUFFER_SIZE - 1);
+
+    evaluate_cmd((const char *)(chunk.buffer));
+    // ChatMessage chat = {};
+    // strcpy(chat.chat, chunk.buffer);
+    //
+    // if (!pb_encode_delimited(&output, ChatMessage_fields, &chat)) {
+    //   fprintf(stderr, "Encoding failed: %s\n", PB_GET_ERROR(&output));
+    // }
+  }
+
+  return NULL;
+}
+
+void *write_output(void *arg) {
+
+  int sockfd = *(int *)(arg);
+  char buffer[BUFFER_SIZE];
+
+  while (1) {
+    bzero(buffer, BUFFER_SIZE);
+    int byte = read(sockfd, buffer, BUFFER_SIZE - 1);
+    if (byte < 0) {
+      break;
+    }
+    printf("%s", buffer);
+  }
+
+  return NULL;
+}
+
+void evaluate_cmd(const char *buffer) {
+  int argc = 0;
+
+  for (int i = 0; buffer[i] != '\0'; i++) {
+    if (buffer[i] == ' ') {
+      argc += 1;
+    }
+  }
+
+  char *argv[argc + 1];
+  int index = 0;
+  int last_index = 0;
+  for (int i = 0; buffer[i] != '\0'; i++) {
+    // if (buffer[i] == ' ') {
+    //   argv[index] = malloc(i - last_index + 1);
+    //   memcpy(argv[i], buffer + last_index, (size_t)(i - last_index + 1));
+    //   index += 1;
+    //   last_index += i;
+    // }
+    if (buffer[i] == ' ') {
+      argv[0] = (char *)malloc(i - last_index + 1);
+      printf("The size is %d", i - last_index + 1);
+      strncat(argv[0], buffer + last_index, i - last_index + 1);
+      // memcpy(argv[i], buffer + last_index, (size_t)(i - last_index + 1));
+      // index += 1;
+      // last_index += i;
+      break;
+    }
+  }
+
+  // for (int i = 0; i <= argc; i++) {
+  printf("The argument at index %d is %s\n", 0, argv[0]);
+  // }
 }
