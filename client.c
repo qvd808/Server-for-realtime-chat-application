8746e03c53161e637a5adb830b1d6b1a2a7d6244
Quang Vinh Dang <dqvinh101@gmail.com>
2024-03-18 23:29:44 -0700

    Update file

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+#include <asm-generic/errno-base.h>
 #include <asm-generic/socket.h>
 #include <dirent.h>
 #include <netinet/in.h>
@@ -98,31 +99,51 @@ void *write_output(void *arg) {
 
 void join(int argc, char *argv[]) {
 
+  if (argc != 2) {
+    printf("Please provide the right format: join room_id\n");
+    return;
+  }
+
   if (argv[1] == NULL || strlen(argv[1]) == 0) {
     printf("Please provide an id for the room\n");
     return;
   }
 
-  printf("argv 1 is %s\n", argv[1]);
+  int room_id = strtol(argv[1], NULL, 10);
+  if (room_id == 0) {
+    printf("Failed to convert. Please provide a valid id\n");
+    return;
+  }
+
+  printf("The room id is %d\n", room_id);
   printf("Joining a room\n");
 }
+
 void create(int argc, char *argv[]) {
 
-  if (argv[1] == NULL || strlen(argv[1]) == 0) {
-    printf("Please provide an id for the room\n");
-    return;
+  if (argc != 1) {
+    printf("Please provide the right format: create\n");
   }
 
-  printf("Creating a romm\n");
+  printf("Please provide password for your room:\n");
+  char buffer[BUFFER_SIZE];
+  int byte = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+
+  if (byte < 0) {
+    printf("Error in creating a room\n");
+    return;
+  }
+  printf("the password is %s\n", buffer);
+  printf("Creating a room\n");
 }
+
 void help(int argc, char *argv[]) {
 
-  if (argv[1] == NULL || strlen(argv[1]) == 0) {
-    printf("Please provide an id for the room\n");
-    return;
+  if (argc != 1) {
+    printf("Please provide the right format: create\n");
   }
-
-  printf("Helpping\n");
+  printf("join <room_id> to join a room\ncreate to create a room to chat with "
+         "your friends\n");
 }
 
 typedef struct {
@@ -130,25 +151,31 @@ typedef struct {
   void (*function)(int argc, char *argv[]);
 } DispatchEntry;
 
-void evaluate_cmd(char *buffer) {
-
-  char *argv[10];
-  int argc = 0;
-  char *token;
-
-  // char *newStr[11];
-  // memcpy(newStr, argv, 88);
-  //
-  // if (newStr[0] != NULL)
-  //   printf("The new string is %s and %s\n", newStr[0], newStr[0] + 7);
-
-  token = strtok(buffer, " \n\0");
+void execute_dispatch_command(int argc, char *argv[]) {
 
   DispatchEntry dispatch_table[] = {
       {"create", create},
       {"join", join},
       {"help", help},
   };
+
+  int numCommand = sizeof(dispatch_table) / sizeof(dispatch_table[0]);
+
+  for (int i = 0; i < numCommand; i++) {
+    if (strcmp(dispatch_table[i].command, argv[0]) == 0) {
+      dispatch_table[i].function(argc, argv);
+      break;
+    }
+  }
+}
+
+void evaluate_cmd(char *buffer) {
+
+  char *argv[10];
+  int argc = 0;
+  char *token;
+
+  token = strtok(buffer, " \n\0");
 
   while (token != NULL) {
     argv[argc] = token;
@@ -159,20 +186,5 @@ void evaluate_cmd(char *buffer) {
     token = strtok(NULL, " \n\0");
   }
 
-  int numCommand = sizeof(dispatch_table) / sizeof(dispatch_table[0]);
-  for (int i = 0; i < numCommand; i++) {
-    if (strcmp(dispatch_table[i].command, argv[0]) == 0) {
-      dispatch_table[i].function(argc, argv);
-      break;
-    }
-  }
-
-  for (int i = 0; i < argc; i++) {
-    memset(argv[i], 0, strlen(argv[i]));
-  }
-
-  // create an enum to pass in
-
-  //   printf("Argument at index %d is %s\n", i, argv[i]);
-  // }
+  execute_dispatch_command(argc, argv);
 }
